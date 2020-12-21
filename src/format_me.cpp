dd70b0ca22f242c123658abba5ef81d051881e9d
Matt Hedley <matthedley@hotmail.co.uk>
2020-12-21 20:17:49 +0000

    Add some c++ files.

---
+++ src/format_me.cpp
@@ -0,0 +1,12 @@
+int exampleFunction(const int x) {
+   return x*   2;
+}
+
+
+
+
+int main() {
+    int i_only_exist_to_be_formatted = 17;;;
+    int me_too = exampleFunction(i_only_exist_to_be_formatted);
+    return me_too;
+}
